ad113ab46d9fffa8a63d82cd0051df3be006775d
Ankesh Bharti <ankesh.b@idsil.com>
2019-09-13 19:53:32 +0530

    PWA-App

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pwa-example'`, () => {
+  it(`should have as title 'pwa-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('pwa-example');
+    expect(app.title).toEqual('pwa-app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to pwa-example!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to pwa-app!');
   });
 });
